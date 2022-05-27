c7d3a358cebd1ad9cd89bb2a7c71a50df84899e7
Rodolfo Rangel <rodolfoarb@gmail.com>
2022-05-26 21:51:57 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,32 +3,46 @@ import axios from 'axios';
 import * as S from "./styled"
 import { useNavigate } from 'react-router-dom';
 
-
 function App() {
-  const [usuario, setUsuario] = useState('');
-  const [erro, setErro] = useState(false);
+  const [nomeUsuario, setNomeUsuario] = useState('');
+  const [erro, setErro] = useState(null);
   const navigate = useNavigate();
+  
+  const handlePesquisa = async () => {
+    
+    try {
+      
+       const response = await axios.get(`https://api.github.com/users/${usuario}/repos`),
+          repositories = response.data.map(repository => repository.name);
 
-  function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`)
-      .then(response => {
-        const repositories = response.data;
-        const repositoriesName = [];
-        repositories.map((repository) => {
-            repositoriesName.push(repository.name)
-        })
-        localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
-        setErro(false)
-        navigate('/repositories');
-      })
-      .catch(() => {
-      setErro(true);
-      });
-  }
+        setErro(null); // tem que ver se desmonta
+    
+        navigate('/repositories', {
+          replace: true,
+          state: {
+            repositories
+        }
+      
+    } catch(error) {
+    
+      setErro(error);
+      
+    }
+   
+  };
+    
+  const updateNomeUsuario = evt => {
+    
+    const nomeUsuario = evt.target.value;
+    
+    setNomeUsuario(nomeUsuario);
+    
+  };
+  
   return (
     <S.HomeContainer>
     <S.Content>
-      <S.Input className='usuario' placeholder='usuário' value={ usuario } onChange={e => setUsuario(e.target.value)}/>
+      <S.Input className='usuario' placeholder='usuário' value={nomeUsuario} onChange={updateNomeUsuario} />
       <S.Button type='button' onClick={handlePesquisa}>Pesquisar</S.Button>
     </S.Content>
       { erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente</S.ErrorMsg> : ''}
